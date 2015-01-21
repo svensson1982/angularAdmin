3d56199f19319415682dbc388303d028d3c42076
unknown <barsib@PROPUB131.pph.lan>
2015-01-21 16:53:02 +0100

    newsletter via directive 2.0

--- app/Controller/newsletter.controller.js
+++ app/Controller/newsletter.controller.js
@@ -11,34 +11,39 @@ angular.module('app')
                 template: "Hírlevél"
             };
         })
-        .directive('dynamic', function () {
+        .directive('dynamic',['$interval', function ($interval) {
             return{
                 restrict: 'EA',
                 replace: true,
                 transclude: true,
                 scope: {
-                    dynamic: "=",
-                    id: '=',
-                    title: '='
+                    dataId: '=',
+                    title: '=',
+                    getnews: "&"
                 },
-                link: function postLink(scope, element, attrs) {
+                link: function (scope, element, attrs) {
                     scope.$watch( function (newVal, oldVal) {
                        /*angular.forEach(scope.title,function(val, key){                          
                             console.log("dynamic2" + scope.title);
                             element.html(val); 
                        });*/
-                        console.log(scope.title);
+                        
                     });
+                    scope.getnews = function(d){alert(d);};//scope.getData();
                 },
-                template: '<a class="btn btn-default btn-block"  ng-repeat="ti in title" >{{ti.title}}|{{ti.id}}</a>'
+                template: '<a class="news-btn btn btn-default btn-block" ng-repeat="ti in title" ng-click="getnews({{ti.id}});" data-id="{{ti.id}}" >{{ti.title}}</a>'
+                //invalid key getnews ng-click
             };
-        });
+        }]);
 
 newsletterController.$inject = ['$scope', '$http', 'loadFactory', 'newsletterFactory', '$location', '$timeout', '$q'];
 
 function newsletterController($scope, $http, loadFactory, newsletterFactory, $location, $timeout, $q) {
 
     //get data by id
+    $scope.getData = function(d){
+        alert("id->"+d);
+    };
     $scope.getDataBack = newsletterFactory.getDataBack;
     var titArr = {};
     $scope.getNews = function () {
